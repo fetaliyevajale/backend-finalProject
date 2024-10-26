dcf50aabe038ddc1a2be8a1bb00e92dad7d89dc8
fetaliyevajale <fetaliyevajale@gmail.com>
2024-10-26 06:41:04 -0700

    AutController/edit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Giriş uğurludursa, admin panelinə yönləndir
+            return redirect()->intended('admin/dashboard');
+        }
+
+        // Giriş uğursuz olarsa, geri qaytar və xətanı göstər
+        return back()->withErrors([
+            'email' => 'Bu email və ya şifrə düzgün deyil.',
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController; // Giriş üçün controller əlavə edin
 
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\AuthController; // Giriş üçün controller əlavə edin
 
+// login formu
 Route::get('admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login.form');
+
+// Admin login 
 Route::post('admin/login', [AuthController::class, 'login'])->name('admin.login');
+
+Route::post('admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
